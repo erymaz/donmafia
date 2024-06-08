b600a44ab13096574676ab5f50ba245d9263c1cc
erymaz <erymaz.murat@gmail.com>
2024-06-09 07:04:43 +0800

    small fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,8 @@
 function Footer() {
   return (
-    <footer className='w-full h-20 py-2'>
+    <footer className='w-full h-16 md:h-20 p-2'>
       <div className='page'>
-        <div className='h-full flex items-center justify-evenly'>
+        <div className='h-full flex items-center justify-between md:justify-evenly'>
           <img src='./img/ft1.png' alt='footer1' className='h-full' />
           <img src='./img/ft2.png' alt='footer2' className='h-full' />
           <img src='./img/ft3.png' alt='footer3' className='h-full' />
